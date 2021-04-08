9de531a057775fe3f39b3cc47689f92069de8066
dillon <dmanhall@gmail.com>
2021-04-08 06:34:58 -0400

    extra functionality on top of base class

--- koolertronSignalGenerator.py
+++ koolertronSignalGenerator.py
@@ -7,7 +7,21 @@ Created on Sat Oct 10 13:39:34 2020
 """
 
 import serial
+import copy
 
+# stories 
+class KoolertronState(object):
+    
+    def __init__(self,freq=None,amplitude=None,offset=None,phase=None,wavetype=None,dutycycle=None):
+        self._freq = freq # hertz
+        self._amplitude = amplitude # volts 
+        self._offSet = offset # volts 
+        self._phase = phase # in radians 
+        self._waveType = wavetype 
+        self._dutycycle = dutycycle
+        
+
+# class for controlling the signal strong
 class KoolertronSig(object):
     
     def __init__(self,ttydev):
@@ -15,20 +29,29 @@ class KoolertronSig(object):
         self.ttydevice = ttydev
         self.baudRate = 115200
         self.endl = b'\r\n'
+        # calles for the states of the channelas
+        self.chan1State = KoolertronState()
+        self.chan2State = KoolertronState()
         
     def searchDevices(self):
         pass
     
+    def setState(self,state):
+        if(type(state) == '__main__.KoolertronState'):
+            pass
+    
     def sendCommand(self,strcmd):
         cmd = str.encode(strcmd)
-        result = False
+        result = None
         #endl = b'\r\n'
         with serial.Serial(self.ttydevice,self.baudRate,timeout=1) as ser:
             ser.write(cmd+self.endl)
             resultCommand = ser.readline()
             # see if the result command works
             if(resultCommand == b':ok\r\n'):
-                result = True
+                result = 'ok'
+            else:
+                result = resultCommand
             
         return result
     
@@ -37,7 +60,7 @@ class KoolertronSig(object):
         pass
         
     # set wave form type,
-    def setWave(self,wavetype,channel):
+    def setWave(self,wavetype,channel=1):
         # serial codes from wave forms 
         wavecommands = {
             "sin" : 0,
@@ -59,53 +82,143 @@ class KoolertronSig(object):
         }
         wavecode = wavecommands[wavetype]
         # build up the command
-        if(channel < 2):
-            strcmd = ':w2{}={}.'.format(channel,wavecode)
-            self.sendCommand(strcmd)
+        strcmd = ':w2{}={}.'.format(channel,wavecode)
+        self.sendCommand(strcmd)
+        # update the state object 
+        if channel == 1:
+            self.chan1State.waveType = wavecommands[wavetype]
+        elif channel == 2:
+            self.chan2State.waveType = wavecommands[wavetype]
     
     # set freq in Hz
-    def setFreq(self,freq,channel):
+    def setFreq(self,freq,channel=1):
         freq = freq * 100
         cmd = None 
         if(channel == 1):
             cmd = ':w23={},0.'.format(int(freq))
+            self.chan1State.freq = freq
         elif(channel == 2):
             cmd = ':w24={},0.'.format(int(freq))
+            self.chan2State.freq = freq
         # send the command back to the server 
         self.sendCommand(cmd)
     
     # set the phase in degrees.  ONly works for channel 2
-    def setPhase(self,phase,channel):
-        pass
+    # phase is a value defined from 
+    def setPhase(self,phase):
+        phaseValue = phase * 100 
+        cmd = ':w31={}.'.format(phaseValue)
+        self.sendCommand(cmd)
+        self.chan2State.phase = phase # only chan 2 has a phase relitive to phase 1
+           
+    # make the truth wave
+    # for testing digital stuff
+    # 
+    def makeTruthWave(self,lspPer,amp=3.3):
+        # figure out the freqency
+        freqCh1 = 1/lspPer 
+        freqCh2 = 2*freqCh1
+        # set the amplitude 
+        self.setAmplitude(amp,1)
+        self.setAmplitude(amp,2)
+        # set the wave type
+        self.setWave("CMOS",1)
+        self.setWave("CMOS",2)
+        # set the freq
+        self.setFreq(freqCh2,1)
+        self.setFreq(freqCh1,2)
+ 
+        
     
     # set the duty cycle
-    def setDuty(self,duty,channel):
-        pass
+    def setDuty(self,duty,channel=1):
+        if(duty <= 1):
+            dutycycle = int(duty * 1000)
+            cmd = None
+            if(channel == 1):
+                cmd = ':w29={}.'.format(dutycycle)
+                self.chan1State.dutycycle = dutycycle
+            elif(channel == 2):
+                cmd = ':w30={}.'.format(dutycycle)
+                self.chan2State.dutycycle = dutycycle
+            self.sendCommand(cmd)
 
     # set the offset voltage
     def setOffSet(self,offset,channel):
-        offsetv = None
+        offsetcmd = None
+        offsetvalue = int(offset*100)
         # set the values for the offset
-        if(offset >= 0):
-            offsetcmd = "1{}".format(int(offset*1000))
+        if(offset > 0):
+            offsetcmd = "1{}".format(str(offsetvalue).zfill(3))
+        elif(offset < 0):
+            offsetvalueN = 1000-abs(offsetvalue)
+            offsetcmd = "{}".format(str(offsetvalueN).zfill(3))
         else:
-            offsetvalue = 1000-offset
-            offsetcmd = "{}".format(int(offsetvalue))
+            offsetcmd = "1000" # one to make the value zero and positive
         # now build up and send the command
+        command = None
+        if(channel == 1):
+            command = ":w27=" + offsetcmd + "."
+            self.chan1State.offSet = offsetcmd
+        elif(channel == 2):
+            command = ":w28=" + offsetcmd + "."
+            self.chan2State.offSet = offsetcmd
+            
+        self.sendCommand(command)
         
+    def pulse(self,amp,highTime,offset=0,phase=0,channel=1):
+        pass
 
     # set the amplitude, amp given in volts 
-    def setAmplitude(self,amp,channel):
+    def setAmplitude(self,amp,channel=1):
         vamp = amp * 1000
         command = None
         if(channel == 1):
             command = ":w25={}.".format(vamp)
+            self.chan1State.amplitude = vamp
         elif(channel == 2):
             command = ":w26={}.".format(vamp)
+            self.chan2State.amplitude = vamp
         # send the command to the system
         self.sendCommand(command)
             
-            
+    def getState(self,channel=1):
+        if channel == 1:
+            return self.chan1State
+        elif channel == 2:
+            return self.chan2State
+        
+    def isConnected(self):
+        cmd = ":r01=0."
+        res = self.sendCommand(cmd)
+        if len(res) > 3:
+            return True 
+        else:
+            return False
+    
+    # helpers for making diffrent functions
+    
+    def sinwave(self,freq,amp,phase=0,offset=0,chan=1):
+        
+        if self.isConnected(): # see if we're connected
+            self.setAmplitude(amp,chan)
+            self.setFreq(freq,chan)
+            self.setWave('sin',chan)
+            self.setOffSet(offset,chan)
+            if chan == 2:
+                self.setPhase(phase)
+        else:
+            return None
+        
+        return copy.copy(self.getState(chan))
 
 class KSchannel(KoolertronSig):
     pass
+
+class Servo(KoolertronSig):
+    
+    def __init__(self,ttydev,chan=1):
+        self.channel = chan
+        super().__init__(ttydev)
+        
+    
--- signaltest.py
+++ signaltest.py
@@ -9,17 +9,12 @@ Created on Sun Oct 11 00:01:40 2020
 # test the signal generator
 from koolertronSignalGenerator import *
 import numpy as np
+from pylab import *
 
 kst = KoolertronSig('/dev/ttyUSB0')
-testfreq = 60e3
-#testfreq = testfreq * 100
-kst.setFreq(testfreq,1)
-kst.setWave('sin',1)
-kst.setAmplitude(7,1)
-#a = ':w23={},0.'.format(int(testfreq))
-#kst.sendCommand(a)
-#kst.sendCommand(':w23=1000,1.')
+# make a wave form
 
-# setup a test sin wave 
-t = np.linspace(0,2*np.pi,1000)
-x1 = np.sin(t)
+kst.setOffSet(0.5,1)
+kst.setOffSet(-3,1)
+
+#kst.setPhase(90)
